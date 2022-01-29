c33d4dff7a353adcb65069142d5f51644f8fb88d
Alex <stukak@gmail.com>
2022-01-29 23:38:44 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,21 +4,24 @@
 echo "deb http://security.debian.org/debian-security jessie/updates main" >> /etc/apt/sources.list
 
 # update package list and install dependencies
-apt update
-apt install -y multiarch-support libavformat57 git libportaudio2* libflac++6v5* libavahi-common3 libavahi-client3 alsa-utils
+apt-get update
+apt-get install -y multiarch-support libssl1.0.0* git libportaudio2* libflac++6v5* libavahi-common3 libavahi-client3 alsa-utils
+
+#more shared libraries for debian buster
+echo "deb https://deb.debian.org/debian/ stretch main contrib non-free" >> /etc/apt/sources.list
+echo "deb https://deb.debian.org/debian/ stretch-updates main contrib non-free" >> /etc/apt/sources.list
+echo "deb http://security.debian.org/debian-security stretch/updates main contrib non-free" >> /etc/apt/sources.list
+
+apt-get update
+apt-get install -y libavformat57
+apt-get install -y libcurl3
 
 # create working folder
 mkdir /usr/ifi
 cd /usr/ifi
 
-# download and install other needed dependencies
-curl -k -O -L http://security.debian.org/debian-security/pool/updates/main/o/openssl/libssl1.0.0_1.0.1t-1+deb8u12_armhf.deb
-apt install -y ./libssl1.0.0_1.0.1t-1+deb8u12_armhf.deb
-curl -k -O -L http://security.debian.org/debian-security/pool/updates/main/c/curl/libcurl3_7.38.0-4+deb8u16_armhf.deb
-apt install -y ./libcurl3_7.38.0-4+deb8u16_armhf.deb
-
 # clone git repo
-git clone https://github.com/shawaj/ifi-tidal-release
+git clone https://github.com/STUkh/ifi-tidal-release
 
 # correct permissions
 chmod +x /usr/ifi/ifi-tidal-release/play
@@ -26,9 +29,9 @@ chmod +x /usr/ifi/ifi-tidal-release/bin/tidal_connect_application
 chmod +x /usr/ifi/ifi-tidal-release/pa_devs/run.sh
 
 # deploy files
-./ifi-tidal-release/file-deploy.sh 
+#./ifi-tidal-release/file-deploy.sh 
 
 # start service and check on status
-systemctl daemon-reload
-systemctl start ifi-streamer-tidal-connect.service 
-systemctl status ifi-streamer-tidal-connect.service
+#systemctl daemon-reload
+#systemctl start ifi-streamer-tidal-connect.service 
+#systemctl status ifi-streamer-tidal-connect.service
